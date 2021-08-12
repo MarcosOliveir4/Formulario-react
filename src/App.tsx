be923abae3cecffe439f5ce735c1a58161b066d1
MarcosOliveira394 <marcosassis071@gmail.com>
2021-08-12 16:51:22 -0300

    :sparkles: Formulario simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Typography } from "@material-ui/core";
+import "./App.css";
+import FormularioCadastro from "./components/formularioCadastro";
+
+type cadastro = {
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  novidade: boolean;
+  promocoes: boolean;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">
+        Formulário de cadastro
+      </Typography>
+      <FormularioCadastro aoEnviar={onSubmit} />
+    </Container>
   );
+}
+
+function onSubmit(dados: cadastro) {
+  console.log(dados);
 }
 
 export default App;
---
+++ src/components/formularioCadastro/formularioCadastro.tsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
+
+type Form = {
+  aoEnviar: Function;
+};
+
+function FormularioCadastro({ aoEnviar }: Form) {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [promocaos, setPromocaos] = useState(true);
+  const [novidades, setNovidades] = useState(true);
+  const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ nome, sobrenome, cpf, novidades, promocaos });
+      }}
+    >
+      <TextField
+        value={nome}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        onChange={(event) => {
+          setNome(event.target.value);
+        }}
+      />
+      <TextField
+        value={sobrenome}
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+      />
+      <TextField
+        value={cpf}
+        id="cpf"
+        error={!erros.cpf.valido}
+        helperText={erros.cpf.texto}
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
+        onBlur={(event) => {
+          const ehValido = validaCPF(event.target.value);
+          setErros(ehValido);
+        }}
+      />
+
+      <FormControlLabel
+        control={
+          <Switch
+            name="promocoes"
+            checked={promocaos}
+            color="primary"
+            onChange={(event) => {
+              setPromocaos(event.target.checked);
+            }}
+          />
+        }
+        label="Promoções"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            name="novidades"
+            checked={novidades}
+            color="primary"
+            onChange={(event) => {
+              setNovidades(event.target.checked);
+            }}
+          />
+        }
+        label="Novidades"
+      />
+
+      <Button variant="outlined" color="primary" type="submit">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+function validaCPF(cpf: string) {
+  if (cpf.length !== 11) {
+    return { cpf: { valido: false, texto: "CPF deve ter 11 digitos" } };
+  } else if (cpf.length < 11) {
+    return { cpf: { valido: false, texto: "CPF deve ter 11 digitos" } };
+  } else {
+    return { cpf: { valido: true, texto: "" } };
+  }
+}
+
+export default FormularioCadastro;
